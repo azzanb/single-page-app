86cec874f0d0b7c0714661d65e7396c9b9ed3b1f
Azzan Braxton <azzanbraxton@gmail.com>
2017-09-20 15:00:46 -0400

    Update

---
+++ public/scripts/controllers/main.js
@@ -0,0 +1,119 @@
+'use strict';
+
+angular.module('app')
+.controller('RecipesController', function($scope, dataService, $location){
+
+	dataService.getCategories(function(response){
+		$scope.getCategories = response.data;
+		$scope.category = $scope.getCategories[0].name;
+	});
+
+	//hide "No recipes found" if recipes are present
+	$scope.hideDiv = false;
+
+	$scope.deleteRecipe = function(){
+		//console.log(this.recipe)
+	};
+
+	$scope.addRecipe = function(){
+		$location.path('/add')
+	}
+
+	dataService.getRecipes(function(response){
+		$scope.getRecipes = response.data;	
+	});
+
+	// dataService.getRecipesId(function(id){
+	// 	$scope.getRecipes = response.data;	
+	// });
+	
+	
+})
+.controller('RecipeDetailController', function($scope, dataService, $location){
+
+	dataService.getCategories(function(response){
+		$scope.getCategories = response.data;
+	});
+
+	//Display recipe's data
+	dataService.getRecipes(function(response){
+		$scope.getRecipes = response.data;	
+		$scope.categories = response.data.category;	
+		for(let i = 0; i < $scope.getRecipes.length; i++){
+			if($location.url() === ('/edit/' + $scope.getRecipes[i]._id)){
+				$scope.recipeName = $scope.getRecipes[i].name;
+				$scope.description = $scope.getRecipes[i].description; 
+				$scope.selected = $scope.getRecipes[i].category;
+				$scope.prepTime = $scope.getRecipes[i].prepTime;
+				$scope.id = $scope.getRecipes[i]._id;
+				$scope.cookTime = $scope.getRecipes[i].cookTime;
+				$scope.ingredients = $scope.getRecipes[i].ingredients;
+				$scope.steps = $scope.getRecipes[i].steps;
+				
+				$scope.postRecipe = function(){
+					dataService.addRecipe($scope.id)
+				}
+			}
+		}
+		if($location.url() === "/add"){
+			$scope.addNew = "Add New Recipe";
+			$scope.recipeName = '';
+			
+		}
+	});
+
+	$scope.cancel = function(){
+		$location.path('/');
+	}
+
+	$scope.updateRecipe = function(data){
+		dataService.updateRecipe({data: $scope.id})
+	}
+
+	$scope.addIngredient = function(){
+		$scope.ingredients.push({
+			foodtItem: "",
+			condition: "",
+			amount: "",
+			$$hashKey: ""
+		});
+	}
+
+	$scope.addStep = function(){
+		if($location.url() !== "/add"){
+			$scope.steps.push({
+				description: "",
+				$$hashKey: ""
+			});
+		}
+		else{
+			$scope.step = {
+				description: ""
+			};
+			$scope.steps.push($scope.step)
+			console.log(this.step)
+		}
+		
+	}
+
+	$scope.deleteStep = function(){
+		const index = this.steps.indexOf(this.step);
+		if(index > -1){
+			this.steps.splice(index, 1);
+		}
+	}
+
+	$scope.deleteIngredient = function() {
+		const index = this.ingredients.indexOf(this.ingredient);
+		if(index > -1){
+			this.ingredients.splice(index, 1);
+		}
+	}
+
+});
+
+
+
+
+
+
---
+++ public/scripts/services/service.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('app')
+.service('dataService', function($http){
+
+	this.getRecipes = function(callback){
+		$http.get('/api/recipes')
+		.then(callback)
+	};
+	this.getCategories = function(callback){
+		$http.get('/api/categories')
+		.then(callback);
+	};
+	this.getFoodItems = function(callback){
+		$http.get('/api/fooditems')
+		.then(callback);
+	};
+	this.getRecipesForCategory = function(category, callback){
+		$http.get('/api/recipes?category={category}')
+		.then(callback);
+	};
+	this.getRecipesId = function(callback, id){
+		$http.get('/api/recipes/{id}')
+		.then(callback);	
+	};
+	this.updateRecipe = function(data, callback, id){
+		$http.put("/api/recipes/{id}", data)
+		.then(callback);
+	};
+	this.addRecipe = function(recipe, callback){
+		$http.post('/api/recipes/')
+		.then(callback);	
+	}
+	this.deleteRecipe = function(callback, id){
+		$http.delete('/api/recipes/{id}')
+		.then(callback);
+	};
+});
